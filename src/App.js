d6721af764578b5447e02b83d6d16ee16cc9fa00
Malek Abdelkader <malekabdelkader2@gmail.com>
2020-10-24 23:46:08 +0100

    another change on app.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
      <button onClick={() => incrementCounter()}>+</button>
       <h2>{counter}</h2>
       <h6> branche "addtest" created by malek </h6>
+      <h6> Hello World  </h6>
+
     </div>
   );
 }
