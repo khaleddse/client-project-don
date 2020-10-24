d2f6f29b4f650c455c7411e738fbd3dcdc3edab8
Riadh <ghribi.riadh1@gmail.com>
2020-10-24 23:06:54 +0200

    add the counter button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
   return (
     <div className="App">
      <h1>project don</h1>
+     <button onClick={() => incrementCounter()}>+</button>
+      <h2>{counter}</h2>
     </div>
   );
 }
