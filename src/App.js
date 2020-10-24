341c5c20a7da42f583d251e33f90ac1244698e9d
selmi khaled <selmikhaled25@gmail.com>
2020-10-25 01:40:29 +0200

    masterclear

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
   return (
     <div className="App">
      <h1>project don</h1>
+<<<<<<< HEAD
      <button onClick={() => incrementCounter()}>+</button>
       <h2>{counter}</h2>
 <<<<<<< HEAD
       
 =======
 >>>>>>> c4da94e (final modifed)
+=======
+     
+>>>>>>> 74151a1 (masterclear)
     </div>
   );
 }
