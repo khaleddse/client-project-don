9207149eeb8657197cf3b76667f19fff91a69d1c
selmi khaled <selmikhaled25@gmail.com>
2020-12-04 18:42:58 +0100

    signup-front

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-
-import './App.css';
-import { useState } from 'react';
-
-function App() {
-  const [counter, setCounter] = useState(0)
-  const incrementCounter = () => {
-    setCounter(counter + 1);
+//import "./App.css";
+import SignupPage from "./pages/Auth/signup";
+import React, { Component, Fragment } from "react";
+import { withRouter, Switch } from "react-router-dom";
+class App extends Component {
+  state = {
+    showBackdrop: false,
+    showMobileNav: false,
+    isAuth: false,
+    token: null,
+    userId: null,
+    authLoading: false,
+    error: null,
+  };
+  signupHandler = (event, authData) => {
+    event.preventDefault();
+    this.setState({ authLoading: true });
+    fetch("http://localhost:5000/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nom: authData.signupForm.nom.value,
+        prenom: authData.signupForm.prenom.value,
+        tel: authData.signupForm.tel.value,
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 404) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (res.status !== 200 && res.status !== 201) {
+          console.log("Error!");
+          throw new Error("Creating a user failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+        this.setState({ isAuth: false, authLoading: false });
+        //this.props.history.replace("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          isAuth: false,
+          authLoading: false,
+          error: err,
+        });
+      });
+  };
+  render() {
+    let routes = (
+      <Switch>
+        <SignupPage
+          onSignup={this.signupHandler}
+          loading={this.state.authLoading}
+        ></SignupPage>
+      </Switch>
+    );
+    return <Fragment>{routes}</Fragment>;
   }
-  return (
-    <div className="App">
-     <h1>project don</h1>
-     
-    </div>
-  );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/pages/Auth/signup.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+
+import Input from "../../components/Form/Input/Input";
+import Button from "../../components/Button/Button";
+import { required, length, email } from "../util/validators";
+import Auth from "./Auth";
+
+class Signup extends Component {
+  state = {
+    signupForm: {
+      nom: {
+        value: "",
+        valid: false,
+        touched: false,
+        validators: [required],
+      },
+      prenom: {
+        value: "",
+        valid: false,
+        touched: false,
+        validators: [required],
+      },
+      tel: {
+        value: "",
+        valid: false,
+        touched: false,
+        validators: [required],
+      },
+      email: {
+        value: "",
+        valid: false,
+        touched: false,
+        validators: [required, email],
+      },
+      password: {
+        value: "",
+        valid: false,
+        touched: false,
+        validators: [required, length({ min: 5 })],
+      },
+      formIsValid: false,
+    },
+  };
+
+  inputChangeHandler = (input, value) => {
+    this.setState((prevState) => {
+      let isValid = true;
+      for (const validator of prevState.signupForm[input].validators) {
+        isValid = isValid && validator(value);
+      }
+      const updatedForm = {
+        ...prevState.signupForm,
+        [input]: {
+          ...prevState.signupForm[input],
+          valid: isValid,
+          value: value,
+        },
+      };
+      let formIsValid = true;
+      for (const inputName in updatedForm) {
+        formIsValid = formIsValid && updatedForm[inputName].valid;
+      }
+      return {
+        signupForm: updatedForm,
+        formIsValid: formIsValid,
+      };
+    });
+  };
+
+  inputBlurHandler = (input) => {
+    this.setState((prevState) => {
+      return {
+        signupForm: {
+          ...prevState.signupForm,
+          [input]: {
+            ...prevState.signupForm[input],
+            touched: true,
+          },
+        },
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Auth>
+        <form onSubmit={(e) => this.props.onSignup(e, this.state)}>
+          <Input
+            id="nom"
+            label="Nom"
+            type="text"
+            control="input"
+            onChange={this.inputChangeHandler}
+            onBlur={this.inputBlurHandler.bind(this, "nom")}
+            value={this.state.signupForm["nom"].value}
+            valid={this.state.signupForm["nom"].valid}
+            touched={this.state.signupForm["nom"].touched}
+          />
+          <Input
+            id="prenom"
+            label="Prenom"
+            type="text"
+            control="input"
+            onChange={this.inputChangeHandler}
+            onBlur={this.inputBlurHandler.bind(this, "prenom")}
+            value={this.state.signupForm["prenom"].value}
+            valid={this.state.signupForm["prenom"].valid}
+            touched={this.state.signupForm["prenom"].touched}
+          />
+          <Input
+            id="tel"
+            label="Numéro de mobile"
+            type="text"
+            control="input"
+            onChange={this.inputChangeHandler}
+            onBlur={this.inputBlurHandler.bind(this, "tel")}
+            value={this.state.signupForm["tel"].value}
+            valid={this.state.signupForm["tel"].valid}
+            touched={this.state.signupForm["tel"].touched}
+          />
+          <Input
+            id="email"
+            label="E-Mail"
+            type="email"
+            control="input"
+            onChange={this.inputChangeHandler}
+            onBlur={this.inputBlurHandler.bind(this, "email")}
+            value={this.state.signupForm["email"].value}
+            valid={this.state.signupForm["email"].valid}
+            touched={this.state.signupForm["email"].touched}
+          />
+
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            control="input"
+            onChange={this.inputChangeHandler}
+            onBlur={this.inputBlurHandler.bind(this, "password")}
+            value={this.state.signupForm["password"].value}
+            valid={this.state.signupForm["password"].valid}
+            touched={this.state.signupForm["password"].touched}
+          />
+          <Button design="raised" type="submit" loading={this.props.loading}>
+            Signup
+          </Button>
+        </form>
+      </Auth>
+    );
+  }
+}
+
+export default Signup;
