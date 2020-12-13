5e09207046cf5417497a159b45b7b081e1bf92b2
selmi khaled <selmikhaled25@gmail.com>
2020-12-13 01:16:48 +0100

    add the Announcement Card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
 import SignupPage from "./pages/Auth/signup";
 import LoginPage from "./pages/Auth/signin";
+<<<<<<< HEAD
+=======
+import AnnouncementsList from "./views/AnnouncementsList/AnnouncementsList";
+//import Annonces from "./pages/Annonce/Annonces";
+//import AddAnnonce from "./pages/Annonce/AddAnonce";
+>>>>>>> e942050 (add the Announcement Card)
 import React, { Component, Fragment } from "react";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 
@@ -16,6 +22,11 @@ class App extends Component {
           path="/signup"
           exact
           render={(props) => <SignupPage {...props} />}
+        />
+        <Route
+          path="/announcements"
+          exact
+          render={(props) => <AnnouncementsList {...props} />}
         />
         <Redirect to="/" />
       </Switch>
---
+++ src/views/AnnouncementsList/AnnouncementsList.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../../services/posts";
+import AnnouncementCard from "../../components/UI/AnnouncementCard/AnnouncementCard";
+import Spinner from "../../components/UI/Spinner/Spinner";
+
+const AnnouncementsList = () => {
+  useEffect(() => {
+    getAllposts();
+  }, []);
+
+  const [announcements, setAnnouncements] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getAllposts = async () => {
+    setIsLoading(true);
+    const result = await getPosts();
+    setAnnouncements(result);
+    setIsLoading(false);
+  };
+  return (
+    <div>
+      {isLoading ? (
+        <Spinner />
+      ) : announcements.length > 0 ? (
+        announcements.map(({ id, name, title, image, created }) => (
+          <AnnouncementCard
+            key={id}
+            name={name}
+            title={title}
+            image={image}
+            created={created}
+          />
+        ))
+      ) : (
+        <h1>No announcments found</h1>
+      )}
+    </div>
+  );
+};
+
+export default AnnouncementsList;
