c907f9086672ad94a5aa38cb7691bdfff4987fb2
selmi khaled <selmikhaled25@gmail.com>
2021-01-06 20:28:27 +0100

    added listAll-user

---
+++ src/views/Admin-UserList/Admin-UserList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import AppBar from "../../components/AppBar/AppBar";
+import Table from "../../components/UI/Table/Table";
+import { getAlluser } from "../../services/UserServices";
+import AppBarMur from "../../components/AppBar/AppBarMur";
+
+const UserList = () => {
+  const [rowstate, setRowState] = useState([]);
+  useEffect(() => {
+    getusers();
+  }, []);
+
+  const getusers = async () => {
+    const users = await getAlluser();
+
+    const result = users.Users.map(({ nom, prenom, email, tel, _id }) => {
+      return {
+        id: _id,
+        firstName: nom,
+        lastName: prenom,
+        email: email,
+        tel: tel,
+      };
+    });
+    setRowState(result);
+  };
+
+  return (
+    <div>
+      <AppBarMur />
+      <Table rows={rowstate} />
+    </div>
+  );
+};
+export default UserList;
