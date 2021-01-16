9f47d696228fd1d346a1ed754e2ac6358199ee13
selmi khaled <selmikhaled25@gmail.com>
2021-01-16 12:54:11 +0100

    ajouter delte methode in liste user

--- src/views/Admin-UserList/Admin-UserList.js
+++ src/views/Admin-UserList/Admin-UserList.js
@@ -2,13 +2,31 @@ import { useState, useEffect } from "react";
 import Table from "../../components/UI/Table/Table";
 import { getAlluser } from "../../services/UserServices";
 import AppBarMur from "../../components/AppBar/AppBarMur";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { delteuser } from "../../services/UserServices";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(20),
+    },
+  },
+}));
 
 const UserList = () => {
   const [rowstate, setRowState] = useState([]);
+  const [formState, setFormState] = useState({
+    values: {
+      userid: "",
+    },
+  });
   useEffect(() => {
     getusers();
   }, []);
-
+  let history = useHistory();
   const getusers = async () => {
     const users = await getAlluser();
 
@@ -23,11 +41,51 @@ const UserList = () => {
     });
     setRowState(result);
   };
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
+    const { userid } = formState.values;
 
+    await delteuser(userid);
+    const rst = rowstate.filter((resultat) => {
+      if (resultat.id !== userid) {
+        return resultat;
+      }
+    });
+    setRowState(rst);
+  };
+  const inputChangeHandler = (e) => {
+    e.persist();
+    setFormState((formState) => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [e.target.name]: e.target.value,
+      },
+    }));
+  };
   return (
     <div>
       <AppBarMur />
       <Table rows={rowstate} />
+      <form
+        className={useStyles.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => submitFormHandler(e)}
+      >
+        <TextField
+          id="filled-secondary"
+          label="Filled secondary"
+          name="userid"
+          variant="filled"
+          color="secondary"
+          value={formState.values.userid}
+          onChange={inputChangeHandler}
+        />
+        <Button variant="contained" color="secondary" type="submit">
+          Supprimer
+        </Button>
+      </form>
     </div>
   );
 };
--- src/views/AnnouncementsList/AnnouncementsList.js
+++ src/views/AnnouncementsList/AnnouncementsList.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { DonContext } from "../../contexte/donContexte";
 import { getPosts } from "../../services/posts";
 import AnnouncementCard from "../../components/UI/AnnouncementCard/AnnouncementCard";
@@ -8,24 +8,23 @@ import { useHistory } from "react-router-dom";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import "../AnnouncementsList/AnnoucementsList.css";
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import Footer from "../../components/Footer/Footer";
-
-
 
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
   heroContent: {
-    backgroundImage: 'url(https://www.pixelstalk.net/wp-content/uploads/2015/01/Snow-winter-landscape-wallpaper-widescreen.jpg)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
+    backgroundImage:
+      "url(https://www.pixelstalk.net/wp-content/uploads/2015/01/Snow-winter-landscape-wallpaper-widescreen.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
     backgroundPosition: "0px -350px",
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
@@ -33,15 +32,15 @@ const useStyles = makeStyles((theme) => ({
   heroButtons: {
     marginTop: theme.spacing(4),
   },
-  main:{
-    backgroundColor:"#f2f2f2",
+  main: {
+    backgroundColor: "#f2f2f2",
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
   footer: {
-    backgroundColor: '#3f51b5',
+    backgroundColor: "#3f51b5",
     padding: theme.spacing(2),
   },
   fab: {
@@ -52,8 +51,7 @@ const useStyles = makeStyles((theme) => ({
     left: "auto",
     position: "fixed",
   },
-}))
-
+}));
 
 export default function AnnouncementsList() {
   const classes = useStyles();
@@ -72,26 +70,32 @@ export default function AnnouncementsList() {
     setIsLoading(true);
     const result = await getPosts();
 
-    setAnnonceHandler(result);
-    setAnnouncements(result.reverse());
+    const annoncefiltred = result.filter((annonce) => {
+      if (annonce.user !== null) {
+        return annonce;
+      }
+    });
+
+    setAnnonceHandler(annoncefiltred);
+    setAnnouncements(annoncefiltred.reverse());
     setIsLoading(false);
   };
 
   const [isfiltred, setIsFiltred] = useState(false);
 
   const [text, setText] = useState({ search: "" });
-  const selectedCateg = async (id,type) => {
+  const selectedCateg = async (id, type) => {
     setIsLoading(true);
-    if(type==="subcateg"){
-    const result = announcementscontexte.filter((annonce) => {
-      if (annonce.subcategid === id) {
-        return annonce;
-      }
-    });
-    setAnnouncements(result);
-  }else if(type==="all"){
-    setAnnouncements(announcementscontexte)
-  }
+    if (type === "subcateg") {
+      const result = announcementscontexte.filter((annonce) => {
+        if (annonce.subcategid === id) {
+          return annonce;
+        }
+      });
+      setAnnouncements(result);
+    } else if (type === "all") {
+      setAnnouncements(announcementscontexte);
+    }
     setIsLoading(false);
   };
 
@@ -109,7 +113,7 @@ export default function AnnouncementsList() {
       history.push("/signin");
     }
   };
-  let i=1;
+  let i = 1;
   const regex = new RegExp(text.search, "i");
   return (
     <React.Fragment>
@@ -122,22 +126,42 @@ export default function AnnouncementsList() {
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Bienvenue 
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Bienvenue
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              <b>Vous avez quelques choses qui vou n'avez plus besoins,vous ne voulez pas le jeté en dehors?<br/>
-              vos gadgets inutiles sont utiles pour quelqun d'autre ,Faites les un don ici en quelques clics !</b>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              <b>
+                Vous avez quelques choses qui vou n'avez plus besoins,vous ne
+                voulez pas le jeté en dehors?
+                <br />
+                vos gadgets inutiles sont utiles pour quelqun d'autre ,Faites
+                les un don ici en quelques clics !
+              </b>
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary" onClick={Auth}>
-                    Créer une annonce 
+                    Créer une annonce
                   </Button>
                 </Grid>
                 <Grid item>
-                  <Button variant="outlined" color="primary" onClick={()=>history.push('contactus')}>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => history.push("contactus")}
+                  >
                     Contacter nous
                   </Button>
                 </Grid>
@@ -148,86 +172,90 @@ export default function AnnouncementsList() {
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
             {isLoading ? (
-          <div className="img-container">
-            <Spinner />
-            <h2>Chargement...</h2>
-          </div>
-        ) : isfiltred ? (
-          announcements
-            .filter((annonces) => {
-              if (regex.test(annonces.objet)) {
-                return annonces;
-              } else {
-                return null;
-              }
-            })
-            .map(
-              ({
-                _id,
-                objet,
-                detail,
-                image,
-                adresse,
-                createdAt,
-                telephone,
-                user,
-              }) => (
-                <Grid item key={i++} xs={12} sm={6} md={4}>
-                <AnnouncementCard
-                  key={_id}
-                  id={_id}
-                  objet={objet}
-                  detail={detail}
-                  createdAt={createdAt}
-                  telephone={telephone}
-                  user={user.nom + " " + user.prenom}
-                  image={image}
-                  adresse={adresse}
-                />
-                </Grid>
+              <div className="img-container">
+                <Spinner />
+                <h2>Chargement...</h2>
+              </div>
+            ) : isfiltred ? (
+              announcements
+                .filter((annonces) => {
+                  if (regex.test(annonces.objet)) {
+                    return annonces;
+                  } else {
+                    return null;
+                  }
+                })
+                .map(
+                  ({
+                    _id,
+                    objet,
+                    detail,
+                    image,
+                    adresse,
+                    createdAt,
+                    telephone,
+                    user,
+                  }) => (
+                    <Grid item key={i++} xs={12} sm={6} md={4}>
+                      <AnnouncementCard
+                        key={_id}
+                        id={_id}
+                        objet={objet}
+                        detail={detail}
+                        createdAt={createdAt}
+                        telephone={telephone}
+                        user={user.nom + " " + user.prenom}
+                        image={image}
+                        adresse={adresse}
+                      />
+                    </Grid>
+                  )
+                )
+            ) : announcements.length > 0 ? (
+              announcements.map(
+                ({
+                  _id,
+                  objet,
+                  detail,
+                  image,
+                  adresse,
+                  createdAt,
+                  telephone,
+                  user,
+                }) => (
+                  <Grid item key={i++} xs={12} sm={6} md={4}>
+                    <AnnouncementCard
+                      key={_id}
+                      id={_id}
+                      objet={objet}
+                      detail={detail}
+                      createdAt={createdAt}
+                      telephone={telephone}
+                      user={user.nom + " " + user.prenom}
+                      image={image}
+                      adresse={adresse}
+                    />
+                  </Grid>
+                )
               )
-            )
-        ) : announcements.length > 0 ? (
-          announcements.map(
-            ({
-              _id,
-              objet,
-              detail,
-              image,
-              adresse,
-              createdAt,
-              telephone,
-              user,
-            }) => (
-              <Grid item key={i++} xs={12} sm={6} md={4}>
-              <AnnouncementCard
-                key={_id}
-                id={_id}
-                objet={objet}
-                detail={detail}
-                createdAt={createdAt}
-                telephone={telephone}
-                user={user.nom + " " + user.prenom}
-                image={image}
-                adresse={adresse}
-              />
-              </Grid>
-            )
-          )
-        ) : (
-          <div className="img-container">
-          <h1>Oops!</h1>
-          <img height="280" width="300" src="https://rivasdev.tech/assets/img/no-data-found.png"/>
-         <h2>Aucune annonce trouvée !</h2>
-         </div>
-        )}
+            ) : (
+              <div className="img-container">
+                <h1>Oops!</h1>
+                <img
+                  height="280"
+                  width="300"
+                  src="https://rivasdev.tech/assets/img/no-data-found.png"
+                />
+                <h2>Aucune annonce trouvée !</h2>
+              </div>
+            )}
           </Grid>
           <Fab color="secondary" className={classes.fab} onClick={Auth}>
-          <AddIcon />
-        </Fab>
+            <AddIcon />
+          </Fab>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </React.Fragment>
   );
 }
